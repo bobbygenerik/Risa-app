4afc32337a090dca18e230878f05b29a7c1b9541
bobbygenerik <bobbyfbrown85@gmail.com>
2025-12-21 15:08:54 +0000

    Fix player focus key event import

--- lib/screens/enhanced_video_player_screen.dart
+++ lib/screens/enhanced_video_player_screen.dart
@@ -1,6 +1,7 @@
 import 'package:iptv_player/utils/debug_helper.dart';
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:media_kit_video/media_kit_video.dart';
